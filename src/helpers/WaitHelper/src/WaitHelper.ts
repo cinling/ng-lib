8950c6e7a64ee48075e9bdd1f68aeb3f398029ba
cinling <1105412681@qq.com>
2021-09-08 11:45:00 +0800

    发布 11.0.1-alpha.1

--- src/helpers/WaitHelper/src/WaitHelper.ts
+++ src/helpers/WaitHelper/src/WaitHelper.ts
@@ -1,4 +1,5 @@
 import {Observable, Subscriber} from "rxjs";
+import {finalize} from "rxjs/operators";
 
 /**
  * 等待工具
@@ -32,7 +33,39 @@ export class WaitHelper {
      * 等待一段时间后再运行
      * @param ms
      */
-    static waitPromise(ms: number): Promise<void> {
+    static waitPromise(ms: number = 100): Promise<void> {
         return WaitHelper.wait(ms).toPromise()
+    }
+
+    /**
+     * 等待直至表达式为 true
+     * @param boolFunction 表达式方法
+     * @param ms 每次判断的等待时长
+     */
+    static whileTrue(boolFunction: () => boolean, ms: number = 100): Observable<void> {
+        const subscriberHandle = (subscriber: Subscriber<void>) => {
+            if (boolFunction()) {
+                subscriber.next()
+                subscriber.complete()
+            } else {
+                setTimeout(() => {
+                    this.whileTrue(boolFunction)
+                        .pipe(finalize(() =>
+                            subscriber.complete()
+                        ))
+                        .subscribe(() => subscriber.next())
+                }, ms)
+            }
+        }
+        return new Observable<void>(subscriberHandle)
+    }
+
+    /**
+     * 等待直至表达式为 true
+     * @param boolFunction 表达式方法
+     * @param ms 每次判断的等待时长
+     */
+    static whileTruePromise(boolFunction: () => boolean, ms: number = 100): Promise<void> {
+        return WaitHelper.whileTrue(boolFunction, ms).toPromise()
     }
 }
